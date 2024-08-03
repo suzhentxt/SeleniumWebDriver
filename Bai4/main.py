0e7c845f3200deb43fa1c8b99fcb55a76d454ede
suzhentxt <chaunguyentotrinh2006@gmail.com>
2024-08-03 18:00:02 +0700

    update

--- Bai4/main.py
+++ Bai4/main.py
@@ -2,3 +2,7 @@ from ChromeController import open_chrome
 
 # open_chrome(name_profile="Profile 1")
 open_chrome()
+
+# cmd:
+#     cd C:\Program Files\Google\Chrome\Application
+#     chrome --remote-debugging-port=4444
