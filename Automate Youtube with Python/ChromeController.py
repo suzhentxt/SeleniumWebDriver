5db3e9c1f0900ba68c240606102152b285c2b588
suzhentxt <chaunguyentotrinh2006@gmail.com>
2024-08-04 04:41:24 +0700

    update

---
+++ Automate Youtube with Python/ChromeController.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+
+class RegYoutubeController():
+
+    def __init__(self) -> None:
+        pass    
+
+    def open_chrome(self, name_profile=None):
+
+        # Mở profile sẵn có
+        chrome_options = Options()
+
+        chrome_options.add_argument("--disable-notifications")
+        chrome_options.add_argument("--disable-infobars")
+
+        if name_profile != None:
+            chrome_options.add_argument("user-data-dir=C:/Users/Admin/AppData/Local/Google/Chrome/User Data")
+            chrome_options.add_argument(f"--profile-directory={name_profile}")
+
+        # chrome_options.add_experimental_option("detach", True)
+        
+        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:4444")
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+        url = "https://www.youtube.com/"
+        self.driver.get(url)
+
+    def watch_video(self, searchtext):
+        # WebDriverWait(driver=self.driver, timeout=20).until(EC.presence_of_element_located((By.XPATH, "//input[@autocomplete='off']"))).click()
+    
+        WebDriverWait(driver=self.driver, timeout=20).until(EC.presence_of_element_located((By.XPATH, "//input[@autocomplete='off']"))).send_keys(searchtext)
+    
+        WebDriverWait(driver=self.driver, timeout=20).until(EC.presence_of_element_located((By.ID, "search-icon-legacy"))).click()
+
+        first_video = WebDriverWait(driver=self.driver, timeout=20).until(EC.element_to_be_clickable((By.XPATH, "//ytd-video-renderer[1]//a[@id='video-title']")))
+        first_video.click()
---
+++ Automate Youtube with Python/main.py
@@ -0,0 +1,7 @@
+from ChromeController import RegYoutubeController
+
+auto = RegYoutubeController()
+
+auto.open_chrome()
+
+auto.watch_video("python")
