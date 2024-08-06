e3ec285ccfa6e6ab754363efb93f3f7c2b35862d
suzhentxt <chaunguyentotrinh2006@gmail.com>
2024-08-06 23:32:39 +0700

    update

--- Bai8/main.py
+++ Bai8/main.py
@@ -5,3 +5,7 @@ auto = RegTikTokController()
 auto.open_chrome()
 
 auto.watch_video(number_video=3)
+
+# cmd:
+#     cd C:\Program Files\Google\Chrome\Application
+#     chrome --remote-debugging-port=4444
